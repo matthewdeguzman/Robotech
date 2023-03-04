3693d1c8c7f8f1912ab846bd391773365f475a21
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-03-04 04:35:03 -0500

    Created navbar, home, and about components

---
+++ frontend/src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( <div> <a href="https://www.linkedin.com/in/matthew-deguzman" target = "_blank" rel="noopener noreferrer">Matthew DeGuzman</a>, <a href="https://www.linkedin.com/in/stephanienguyen25/" target="_blank" rel="noopener noreferrer">Stephanie Nguyen</a>, and <a href="https://www.linkedin.com/in/alex-huper/" target = "_blank" rel="noopener noreferrer">Alexander Huper</a> from the University of Florida 
+        have decided to come together and collaborate to make their biggest project yet, "Schedule Recommender (Name TBA)"
+        for the Robotech hackathon hosted by Georgia Tech IEEE. They seek to improve the mental health of students worldwide
+        by devising a website that recommends users a priority-based schedule that tells them what events to complete
+        based on factors such as their due date and duration. Together, they are a force to reckon with and many are exhilerated
+        and filled with excitement to see the end result.</div> );
+}
+ 
+export default About;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Schedule Recommender (Name TBA)</h1>
+            <div className="links">
+                <a className="home" href="/">Home</a>
+                <a className="about" href="/about">About us</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
