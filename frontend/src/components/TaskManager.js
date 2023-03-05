fa2aac1cad04c3af7c4d33a462b511f69a6c9f99
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-03-04 22:59:07 -0500

    made events forms to edit now

--- frontend/src/components/TaskManager.js
+++ frontend/src/components/TaskManager.js
@@ -1,14 +1,13 @@
-import TaskList from "./TaskList";
+import Task from './Task';
 import {useState} from 'react'
 const uuidv4 = require('uuid').v4;
 
 const TaskManager = () => {
-    const[tasks, setTasks] = useState([
-        
-    ]);
+    const[tasks, setTasks] = useState([]);
 
     function addEvent() {
-        setTasks([...tasks, {name: "", dueDate: "", priority: "", duration: "", description: "", number: uuidv4()}])
+        const date = new Date();
+        setTasks([...tasks, {name: "Enter name", dueDate: date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear(), priority: "10", duration: "01:00:00", description: "", number: uuidv4()}])
     }
 
     function addAll() {
@@ -18,7 +17,9 @@ const TaskManager = () => {
         <>
             <button className="add-event" onClick={addEvent}>Add Event</button>
             <div className="scroll-box">
-                {tasks && <TaskList tasks={tasks}/>}
+                <div className="task-list">
+                    {tasks && tasks.map((task) => ( <Task key={task.number} task={task}/>))}
+                </div>
             </div>
             <button className="add-all" onClick={addAll}> Add All Events</button>
         </>
